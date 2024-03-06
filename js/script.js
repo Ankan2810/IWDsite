ba7e66142257c62963dc6d98c6e2b04326bf6090
Ankan2810 <22130111@st.hcmuaf.edu.vn>
2024-03-06 20:35:14 +0700

    .

--- js/script.js
+++ js/script.js
@@ -75,26 +75,26 @@ function signUp(e) {
         case "Mom" :
             if(password ==="271272") {
                 alert("Đăng nhập thành công! Helu mẹ mìn");
-                window.location.href = "../mom.html";
+                window.location.href = "../mom/mom.html";
                 break;
             }
         case "Sis" :
             if(password ==="090595"){
                 alert("Login Successful! Hello sít tờ");
-                window.location.href = "../sis.html";
+                window.location.href = "../sis/sis.html";
                 break;  
             }
         case "Bae" :
             if(password ==="040600"){
                 alert("Login Successful! Hello bấy bì");
-                window.location.href = "../babe.html";
+                window.location.href = "../babe/babe.html";
                 break;  
 
             }
         case data.username :
             if(password === data.password){
                 alert("Login Successful!");
-                window.location.href = "../woman.html";
+                window.location.href = "../woman/woman.html";
                 break;  
 
             }
